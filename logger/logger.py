485233aabea8b07694a7a436932e616d6d394625
Connor Wenck <none>
2016-03-06 20:44:21 +0000

    script seems to be working

--- logger/logger.py
+++ logger/logger.py
@@ -11,7 +11,21 @@ import re
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
+DEBUG = True
+
+GROUPME_ENABLED = True
+GSPREAD_ENABLED = True
+
 HPOT_IDS = [100, 200]
+HPOT_IPS = {
+    100: '8.8.8.8',
+    200: '8.8.8.4'
+}
+
+ALLOWED_MINS = 0.25
+
+GDRIVE_CHANGELOG_SPREAD_KEY = None
+GDRIVE_CONNECTION_SPREAD_KEYS = None
 
 # These paths are relative to the root of the host
 HPOT_ROOT_LOCATION = '/vz/private/'
@@ -21,19 +35,39 @@ HSSH_ROOT = '/usr/games/.apps/honssh/'
 ACTIVE_HPOT_FILE = os.path.join(STORAGE_LOCATION, 'active_honeypots')
 
 # These paths are relative to the root of the honeypot
-LOG_FILES = ['/var/log/auth.log', '/usr/games/.logs/valid_passwords.log']
+LOG_FILES = ['/var/log/auth.log', '/usr/games/.logs/valid_passwords.log', '/usr/games/.logs/snoopy.log', '/usr/games/.logs/logger.log']
 HSSH_LOG_DIRS = []
 
 # This should be left as None
 STATUS_BOT_ID = None
 
-def authorize_client():
-    credential_path = os.path.dirname(os.path.realpath(__file__)) + '/credentials.json'
-    scope = ['https://spreadsheets.google.com/feeds']
-    credentials = ServiceAccountCredentials.from_json_keyfile_name(credential_path, scope)
-    auth = gspread.authorize(credentials)
-    print('Completed authorization.')
-    return auth
+GSPREAD_CLIENT = None
+
+def auth_client():
+    global GSPREAD_CLIENT
+
+    if GDRIVE_CHANGELOG_SPREAD_KEY == None:
+        __gspread_init()
+    
+    if GSPREAD_CLIENT == None:
+        credential_path = os.path.dirname(os.path.realpath(__file__)) + '/credentials.json'
+        scope = ['https://spreadsheets.google.com/feeds']
+        credentials = ServiceAccountCredentials.from_json_keyfile_name(credential_path, scope)
+        auth = gspread.authorize(credentials)
+        GSPREAD_CLIENT = auth
+        print('Completed authorization.')
+    
+    return GSPREAD_CLIENT
+
+def __gspread_init():
+    global GDRIVE_CHANGELOG_SPREAD_KEY, GDRIVE_CONNECTION_SPREAD_KEYS
+    
+    with open('gspread_keys.txt', 'r') as f:
+        GDRIVE_CHANGELOG_SPREAD_KEY = f.readline().replace('\n', '')
+        GDRIVE_CONNECTION_SPREAD_KEYS = {}
+        for hp_id in HPOT_IDS:
+            GDRIVE_CONNECTION_SPREAD_KEYS[hp_id] = f.readline().replace('\n', '')
+            
 
 
 def shell_cmd(cmd):
@@ -49,6 +83,9 @@ def shell_cmd_oneline(cmd):
 
 
 def groupme_log(message):
+    if not GROUPME_ENABLED:
+        return
+
     if STATUS_BOT_ID == None:
         __groupme_init()
     
@@ -56,10 +93,12 @@ def groupme_log(message):
 
 
 def __groupme_init():
-    """ Reads the file groupme_bot.txt to determine the BOT ID to use for logging.
+    """ Reads the file botid.txt to determine the BOT ID to use for logging.
     """
-
     global STATUS_BOT_ID
+
+    if not GROUPME_ENABLED:
+        return
 
     with open('botid.txt', 'r') as f:
         STATUS_BOT_ID = f.readline().replace('\n', '')
@@ -79,8 +118,27 @@ def find_next_row(worksheet):
         row_num += 1
     return None
 
+def write_session_open(worksheet, open_connection):
+    row = find_next_row(worksheet)
+    if row is None:
+        print('Out of rows. Adding 100 more.')
+        worksheet.add_rows(100)
+        row = find_next_row(worksheet)
 
-def write_entry(worksheet):
+    row_cells = worksheet.range('A' + str(row) + ':E' + str(row))
+    row_cells[0].value = std_format_date(open_connection['timestamp'])
+    row_cells[2].value = open_connection['ip']
+    row_cells[3].value = open_connection['uname']
+    row_cells[4].value = open_connection['passwd']
+    worksheet.update_cells(row_cells)
+    
+
+    
+def write_session_close(worksheet, open_connection, close_connection):
+    worksheet.update_cell(open_connection['log_line'], 2, std_format_date(close_connection['timestamp']))
+
+
+def write_changelog_entry(worksheet, event_type, detail_msg):
     """ Trys to log the next entry in the worksheet.
     """
     row = find_next_row(worksheet)
@@ -88,14 +146,13 @@ def write_entry(worksheet):
         print('Out of rows. Adding 100 more.')
         worksheet.add_rows(100)
         row = find_next_row(worksheet)
-    print('Writing data to row ' + str(row) + '.')
-    row_cells = worksheet.range('A' + str(row) + ':C' + str(row))
-    row_cells[0].value = shell_cmd('date')
-    row_cells[1].value = disk_space()
-    row_cells[2].value = 'test'
+
+    row_cells = worksheet.range('A' + str(row) + ':D' + str(row))
+    row_cells[0].value = event_type
+    row_cells[1].value = 'SCRIPT'
+    row_cells[2].value = std_format_date(now_timestamp())
+    row_cells[3].value = detail_msg
     worksheet.update_cells(row_cells)
-    print('Row written.')
-    return True
 
 #line = shell_cmd('date') + ',' + disk_space() + ',' + shell_cmd('uptime | cut -d" " -f11')
 
@@ -148,7 +205,6 @@ def gen_honeypot_root_dirs():
         
         if not os.path.exists(hssh_store):
             os.makedirs(hssh_store)
-            
         
         #Add the directories for the session folder in honSSH
         hssh_root = re.sub(r'^/', '', HSSH_ROOT)
@@ -194,7 +250,7 @@ def update_tracked_files(paths):
         #base_log_paths.append(os.path.join(default_store, os.path.basename(log) + '.default'))        
         log_paths.append({
             'src': os.path.join(paths['hp_root'], log),
-            'dest': os.path.join(paths['curr_session_store'], os.path.basename(log) + '.bk')
+            'dest': os.path.join(paths['curr_session_store'], os.path.basename(log))
         })
 
     for log_dir in HSSH_LOG_DIRS:
@@ -297,7 +353,7 @@ def rsyslog_parse_line(hp_id, text):
     if re.search(r'[a-zA-Z]+\[[0-9]+\]:', text) != None:
         # Syslogtag has a pid number, so parse it out of the program
         program, pid = program.split('[')
-        
+               
         #Remove the closing ']'
         pid = pid[:-1]
 
@@ -364,7 +420,7 @@ def hssh_auth_parse_line(hp_id, text):
         'ip': ip,
         'uname': username,
         'passwd': passwd,
-        'success': success == 'True'
+        'success': success == 'True',
     }
 
     return line
@@ -375,8 +431,12 @@ def hssh_open(parsed_line, open_connections):
         return open_connections
 
     if parsed_line['success']:
+        gc = auth_client()
+        spread = gc.open_by_key(GDRIVE_CONNECTION_SPREAD_KEYS[parsed_line['hp_id']])
+        parsed_line['log_line'] = find_next_row(spread.sheet1)
         open_connections.append(parsed_line)
         hssh_log_open(parsed_line)
+
     else:
         hssh_log_open_fail(parsed_line)
         
@@ -399,14 +459,39 @@ def hssh_close(parsed_line, open_connections):
 def hssh_log_open(open_connection):
     print('Connection opened to ' + open_connection['ip'] + ' at ' + format_time(open_connection['timestamp']) + ' with ' + open_connection['uname'] + ':' + open_connection['passwd'])
 
+    gc = auth_client()
+    
+    spread = gc.open_by_key(GDRIVE_CHANGELOG_SPREAD_KEY)
+    wksht = spread.sheet1
+    write_changelog_entry(wksht, 'Compromise Detected', 'Attacker gained access with password ' + open_connection['passwd'])   
+
+    spread = gc.open_by_key(GDRIVE_CONNECTION_SPREAD_KEYS[open_connection['hp_id']])
+    wksht = spread.sheet1
+    write_session_open(wksht, open_connection)
+
+    groupme_log('Compromise: Attacker gained access with ' + open_connection['uname'] + ':' + open_connection['passwd']])
+
 def hssh_log_open_fail(open_connection):
     print('Failed to connect to ' + open_connection['ip'] + ' at ' + format_time(open_connection['timestamp']) + ' with ' + open_connection['uname'] + ':' + open_connection['passwd'])
+
 
 def hssh_log_close(open_connection, close_connection):
     print('Connection closed to ' + close_connection['ip'] + ' at ' + format_time(close_connection['timestamp']) + ' opened at ' + format_time(open_connection['timestamp']))
 
+    gc = auth_client()
+
+    spread = gc.open_by_key(GDRIVE_CHANGELOG_SPREAD_KEY)
+    wksht = spread.sheet1
+    write_changelog_entry(wksht, 'Compromise Resolved', 'Attacker ended session started on ' + std_format_date(open_connection['timestamp']))
+
+    spread = gc.open_by_key(GDRIVE_CONNECTION_SPREAD_KEYS[open_connection['hp_id']])
+    wksht = spread.sheet1
+    write_session_close(wksht, open_connection, close_connection)
+ 
+
 def format_date(datetime_obj):
     return datetime_obj.strftime('%a %b %d, %Y')
+
 
 def format_time(datetime_obj):
     return datetime_obj.strftime('%I:%M:%S %p')
@@ -450,8 +535,10 @@ def honeypot_logger():
 
     hssh_open_connections = {}
     been_compromised = {}
+    session_start_time = {}
 
     session_ip = {}
+    force_kick = {}
 
     if not os.path.exists(ACTIVE_HPOT_FILE):
         with open(ACTIVE_HPOT_FILE, 'w') as f:
@@ -461,6 +548,8 @@ def honeypot_logger():
     for hp_id in HPOT_IDS:
         hssh_open_connections[hp_id] = []
         session_ip[hp_id] = None
+        session_start_time[hp_id] = None
+        force_kick[hp_id] = False
         been_compromised[hp_id] = False    
 
 
@@ -470,16 +559,25 @@ def honeypot_logger():
         prev_active_hpots = active_hpots
         active_hpots = read_active_honeypots()
         
-        
-        
+        print(active_hpots)       
         for tmp_hp_id in active_hpots:
             if tmp_hp_id not in prev_active_hpots:
                 tmp_hp_id = int(tmp_hp_id)
                 archive_session(tmp_hp_id, session_ip[tmp_hp_id], paths[tmp_hp_id]['storage_root'])
                 session_ip[tmp_hp_id] = None
+                firewall_reset(tmp_hp_id)
+                start_honssh(tmp_hp_id)
 
         #for hp_id in active_hpots:
         hp_id = 100
+
+        if session_start_time[hp_id] != None:
+            time_diff = session_start_time[hp_id] - now_timestamp()
+            print(abs(time_diff).total_seconds())
+            if abs(time_diff).total_seconds() >= (ALLOWED_MINS * 60):
+                print('Kicking all sessions for honeypot ' + str(hp_id))
+                stop_honssh(hp_id)
+                force_kick[hp_id] = True
 
         log_paths = update_tracked_files(paths[hp_id])
         for log in log_paths:
@@ -490,16 +588,19 @@ def honeypot_logger():
                 
                 if (not been_compromised[hp_id]) and len(hssh_open_connections[hp_id]) > 0:
                     session_ip[hp_id] = hssh_open_connections[hp_id][0]['ip']
+                    session_start_time[hp_id] = now_timestamp()
+                    firewall_whitelist(hp_id, session_ip[hp_id])
                     been_compromised[hp_id] = True
                 
                 hssh_open_connections[hp_id] = hssh_close(parsed_line, hssh_open_connections[hp_id])
-                
 
-        if len(hssh_open_connections[hp_id]) == 0 and been_compromised[hp_id]:
+        if (len(hssh_open_connections[hp_id]) == 0 and been_compromised[hp_id]) or force_kick[hp_id]:
+            force_kick[hp_id] = False
             recycle(hp_id)
             been_compromised[hp_id] = False
+            session_start_time[hp_id] = None
         sleep(1)
-
+        
 
 def recycle(hp_id):
     hp_id = str(hp_id)
@@ -510,8 +611,36 @@ def recycle(hp_id):
             if hp_id != old_hp_id:
                 f.write(old_hp_id + '\n')
 
-    # Actaully recycle the box here
-    pass
+    if not DEBUG:
+        shell_cmd_oneline('restoreHoneypot ' + str(hp_id))
+
+def firewall_whitelist(hp_id, ip_addr):
+    list_dir = '/home/hp/firewall-whitelist'
+
+    if not os.path.exists(list_dir):
+        os.makedirs(list_dir)
+
+    with open(os.path.join(list_dir, HPOT_IPS[hp_id]), 'w') as f:
+        f.write(ip_addr + '\n')
+    
+    print('Whitelisting ip ' + ip_addr + ' for honeypot ' + str(hp_id))
+    
+    if not DEBUG:
+        shell_cmd_oneline('runFirewall')
+
+
+def firewall_reset(hp_id):
+    list_dir = '/home/hp/firewall-whitelist'
+    file_path = os.path.join(list_dir, HPOT_IPS[hp_id])
+    
+    if os.path.exists(file_path):
+        os.remove(file_path)
+
+    print('Resetting firewall for honeypot ' + str(hp_id))
+    
+    if not DEBUG:
+        shell_cmd_oneline('runFirewall')
+
 
 def archive_session(hp_id, session_ip, storage_root):
     print('Archiving session for honeypot ' + str(hp_id) + ' from ip ' + session_ip)
@@ -535,6 +664,41 @@ def archive_session(hp_id, session_ip, storage_root):
     shell_cmd_oneline('mv ' + os.path.join(hssh_log_dir, '*') + ' ' +  session_dir + '; rmdir ' + hssh_log_dir)
     shell_cmd_oneline('rmdir ' + hssh_dir + ' > /dev/null 2>&1 || mv ' + hssh_dir + ' ' +  os.path.join(session_dir, 'additional_ip_logins'))
 
+def start_honssh(hp_id):
+    print('Starting honssh on honeypot ' + str(hp_id))
+
+    if DEBUG:
+        shell_cmd_oneline('cd /usr/games/.apps/honssh/ && ./honsshctrl.sh start')
+    else:
+        shell_cmd_oneline('vzctl exec 100 ' + os.path.join(HSSH_ROOT, 'honsshctrl.sh') + ' start')
+    clear_logs(hp_id)
+
+def stop_honssh(hp_id):
+    print('Stopping honssh on honeypot ' + str(hp_id))
+    
+    if DEBUG:
+        shell_cmd_oneline('echo -n "The system is going down for halt NOW!" | wall > /dev/null 2>&1')
+        shell_cmd_oneline('cd /usr/games/.apps/honssh/ && ./honsshctrl.sh stop')
+    else:
+        shell_cmd_oneline('vzctl exec 100 echo -n "The system is going down for halt NOW!" | wall 2>&1')
+        shell_cmd_oneline('vzctl exec 100 ' + os.path.join(HSSH_ROOT, 'honsshctrl.sh') + ' stop')
+
+
+def clear_logs(hp_id):
+    print('Clearing logs for heonypot ' + str(hp_id))
+    if not DEBUG:
+        shell_cmd_oneline('log-clear ' + str(hp_id) + ' > /dev/null 2>&1')
+
+def std_format_date(datetime_obj):
+    return datetime_obj.strftime('%a %b %m %H:%M:%S %Z %Y')
+
+
+def now_timestamp():
+    time = datetime.now()
+    timezone_str = shell_cmd_oneline('date +"%Z"')
+    time = time.replace(tzinfo=tz.gettz(timezone_str))
+    return time.astimezone(tz.gettz('EST'))
+
 def print_paths():
     import pprint
     pp = pprint.PrettyPrinter(indent=4)
@@ -543,4 +707,14 @@ def print_paths():
         files = update_tracked_files(paths[hp_id])
         pp.pprint(files)
     
-honeypot_logger()
+
+try:
+    if DEBUG:
+        print('WARNING: Running in debug mode.')
+    honeypot_logger()
+except KeyboardInterrupt:
+    print('Stopping...')
+except:
+    groupme_log('Fatal Error: Script Terminating')
+    print('Fatal Error: Script Terminating')
+    raise
